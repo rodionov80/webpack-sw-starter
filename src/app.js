ad7d67a91a1cc49d42a21ead68eabf2b41640033
rodionoff <rodionov134@gmail.com>
2018-09-25 23:45:55 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,1 @@
+// import you .js files here
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/app.js',
+    './src/sass/style.sass'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+   ]    
+};
